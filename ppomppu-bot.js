2a538cfcbfd5456e6c4f5bf93fd8ceef9e671bfa
mildobread <mildobread@gmail.com>
2024-02-09 00:03:08 +0900

    indent
    Signed-off-by: mildobread <mildobread@gmail.com>

--- ppomppu-bot.js
+++ ppomppu-bot.js
@@ -3,23 +3,22 @@ kakao_rooms = []
 
 function onNotificationPosted(sbn, sm) {
     var packageName = sbn.getPackageName();
-	if (packageName.startsWith("org.telegram.messenger")) {
-		var actions = sbn.getNotification().actions;
-		if (actions == null) return;
-		var userId = sbn.getUser().hashCode();
-		for (var n = 0; n < actions.length; n++) {
-			var action = actions[n];
-			if (action.getRemoteInputs() == null) continue;
-			var bundle = sbn.getNotification().extras;
-
-			var msg = bundle.get("android.text").toString();
-			if (!(msg.includes('해피머니') || msg.includes('컬쳐랜드'))) return;
-			for (var i = 0; i < kakao_replier.length; i++) {
-				kakao_replier[i].reply(msg);
-			}
-		}
-		return;
-	}
+    if (packageName.startsWith("org.telegram.messenger")) {
+        var actions = sbn.getNotification().actions;
+        if (actions == null) return;
+        var userId = sbn.getUser().hashCode();
+        for (var n = 0; n < actions.length; n++) {
+            var action = actions[n];
+            if (action.getRemoteInputs() == null) continue;
+            var bundle = sbn.getNotification().extras;
+            var msg = bundle.get("android.text").toString();
+            if (!(msg.includes('해피머니') || msg.includes('컬쳐랜드'))) return;
+            for (var i = 0; i < kakao_replier.length; i++) {
+                kakao_replier[i].reply(msg);
+            }
+        }
+        return;
+    }
     if (!packageName.startsWith("com.kakao.tal")) return;
     var actions = sbn.getNotification().actions;
     if (actions == null) return;
@@ -28,7 +27,6 @@ function onNotificationPosted(sbn, sm) {
         var action = actions[n];
         if (action.getRemoteInputs() == null) continue;
         var bundle = sbn.getNotification().extras;
-
         var msg = bundle.get("android.text").toString();
         var sender = bundle.getString("android.title");
         var room = bundle.getString("android.subText");
@@ -54,28 +52,28 @@ function onNotificationPosted(sbn, sm) {
 }
 
 function responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName, isMultiChat) {
-	if (!msg.startsWith('.')) return;
+    if (!msg.startsWith('.')) return;
 
-	message = '';
-	switch (msg) {
-		case '.상테크알림':
-			if (kakao_rooms.includes(room)) {
-				message += '이미 등록되어있음'
-			}
-			else {
-				kakao_replier.push(replier);
-				kakao_rooms.push(room);
-				message += '등록완료';
-			}
-			break;
-		case '.목록':
-			for (let i = 0; i < kakao_rooms.length; i++) {
-				message += kakao_rooms[i];
-				if (i != kakao_rooms.length - 1) {
-					message += '\n';
-				}
-			}
-			break;
-		}
-	replier.reply(message)
+    message = '';
+    switch (msg) {
+        case '.상테크알림':
+            if (kakao_rooms.includes(room)) {
+                message += '이미 등록되어있음'
+            }
+            else {
+                kakao_replier.push(replier);
+                kakao_rooms.push(room);
+                message += '등록완료';
+            }
+            break;
+        case '.목록':
+            for (let i = 0; i < kakao_rooms.length; i++) {
+                message += kakao_rooms[i];
+                if (i != kakao_rooms.length - 1) {
+                    message += '\n';
+                }
+            }
+            break;
+        }
+    replier.reply(message)
 }
